dce6ded85f6412fd13cbd100c5908587cfe26e1b
Iqbal Fasri <iqbalx64@gmail.com>
2019-11-30 13:16:46 +0700

    Updates

---
+++ precache-manifest.3bce69ef172675bd6b5f8736875c7b68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324c0ff18c0a18f906408bc30623695b",
+    "url": "/furniture-finder/index.html"
+  },
+  {
+    "revision": "e8bba5e5a490ca9a597d",
+    "url": "/furniture-finder/static/css/main.661a6992.chunk.css"
+  },
+  {
+    "revision": "39afceb9a951f6710860",
+    "url": "/furniture-finder/static/js/2.b0ba3820.chunk.js"
+  },
+  {
+    "revision": "e8bba5e5a490ca9a597d",
+    "url": "/furniture-finder/static/js/main.9ecc05dd.chunk.js"
+  },
+  {
+    "revision": "871d92654a54ac19b311",
+    "url": "/furniture-finder/static/js/runtime-main.cd44dbdd.js"
+  }
+]);
